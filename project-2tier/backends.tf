5ab5a934e424083bf0a6982a7bcfbdc1c4f723e3
Joseph Peter <josephvpeter7@gmail.com>
2023-01-21 23:10:42 -0500

    update structure

---
+++ project-2tier/backends.tf
@@ -0,0 +1,12 @@
+# --- remote/backends.tf ---
+
+# enable to run through cloud
+# terraform {
+#   backend "remote" {
+#     hostname     = "app.terraform.io"
+#     organization = "CompanyName1"
+#     workspaces {
+#       name = "TwoTier-aws"
+#     }
+#   }
+# }
